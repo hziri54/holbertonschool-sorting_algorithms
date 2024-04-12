2752a9b2e4efb459e4a7cfbf4176842e9965be61
hziri54 <zirihouamdi64@gmail.com>
2024-04-12 11:32:13 +0000

    question 1 fini, Écrire une fonction qui trie une liste doublement chaînée d'entiers dans l'ordre croissant en utilisant l'algorithme de tri par insertion.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - fonction qui trie une liste doublement chaînée d'entiers
+ * dans l'ordre croissant en utilisant l'algorithme de tri par insertion
+ *
+ * @list : un pointeur sur la tête d'une liste double
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *prev;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current->next;
+		prev = current->prev;
+		while (prev != NULL && prev->n > current->n)
+		{
+			prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			prev->prev = current;
+			if (current->prev != NULL)
+				current->prev->next = current;
+			else
+				*list = current;
+			prev = current->prev;
+			print_list(*list);
+		}
+
+		current = temp;
+	}
+}
